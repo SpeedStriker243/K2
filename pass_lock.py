80e6dcd16955da02966b46fe9a7c4c001d8079f9
Arsalan <sonicspeed848@gmail.com>
2020-11-09 20:57:27 +0000

    Create pass_lock.py

---
+++ pass_lock.py
@@ -0,0 +1,10 @@
+import os
+
+class PassNotFound(Exception)
+    pass
+
+def CheckForPass(filetocheck):
+    if os.path.exists(filetocheck):
+        pass
+    else:
+        raise PassNotFound("The specified pass was not found.")
