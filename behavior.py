445a9c9fc3b54bc4ada737796d7db3d6e8537eb7
Arsalan <sonicspeed848@gmail.com>
2020-11-09 20:06:46 +0000

    Create behavior.py

---
+++ behavior.py
@@ -0,0 +1,7 @@
+while Awake == True:
+    print("K2 active")
+    Twitter.Login("@yaboiK2")
+    Discord.Login("K2#9711", 521830344937308160)
+    Person.Intelligence.Enable()
+    if Time > 0000:
+      break
