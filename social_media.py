de0859f76a99c9386d37cb7a86cfaa432b8e64a9
Arsalan <sonicspeed848@gmail.com>
2020-11-09 20:32:39 +0000

    Create social_media.py

---
+++ social_media.py
@@ -0,0 +1,6 @@
+class Twitter:
+    def Login(name):
+        print("Logged into Twitter as " + name)
+class Discord:
+    def Login(username, tag, id):
+        print("Logged into Discord as " + username + "#" + tag + " (ID " + id + ")")
