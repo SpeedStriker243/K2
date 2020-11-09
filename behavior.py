fea16f16e6e9b81c8cbab531da7130ca4f9b265d
Arsalan <sonicspeed848@gmail.com>
2020-11-09 20:43:55 +0000

    Update behavior.py

--- behavior.py
+++ behavior.py
@@ -1,7 +1,11 @@
+import social_media as SocialMedia
+import human_functions as Person
+from datetime import datetime
+
 while Awake == True:
     print("K2 active")
-    Twitter.Login("@yaboiK2")
-    Discord.Login("K2#9711", 521830344937308160)
-    Person.Intelligence.Enable()
-    if Time > 0000:
+    SocialMedia.TwitterLogin("@yaboiK2")
+    SocialMedia.DiscordLogin("K2#9711", 521830344937308160)
+    Person.Intelligence.Enable = true
+    if datetime.current_time.hour > 11 and datetime.current_time.minute > 59:
       break
